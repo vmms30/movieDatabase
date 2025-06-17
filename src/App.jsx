ea1a02e849d468f51020e8581e4fa013bde8dd90
Vincent-Soweto-biz <vincent.soweto.biz@gmail.com>
2025-06-17 17:05:15 +0200

    update routes to include favorites page

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,29 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Container } from 'react-bootstrap';
-import Home from './pages/Home';
-import Favourites from './pages/Favourites';
-import HomePage from './pages/HomePage';
-import MovieDetailsPage from './pages/MovieDetailsPage';
-import Navigation from './components/Navigation';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import { Container } from "react-bootstrap";
+import Home from "./pages/Home";
+import FavouritesPage from "./pages/FavouritesPage";
+import HomePage from "./pages/HomePage";
+import MovieDetailsPage from "./pages/MovieDetailsPage";
+import Navigation from "./components/Navigation";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    <Container fluid >
-      <Router>
-        
-          <Navigation /> 
+      <Container fluid>
+        <Router>
+          <Navigation />
           <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path='/favourites' element={<Favourites/>}/>
+            <Route path="/favourites" element={<FavouritesPage />} />
             <Route path="/trending" element={<Home />} />
             <Route path="/movie/:movieId" element={<MovieDetailsPage />} />
             {/* Add 404 route: <Route path="*" element={<NotFound />} /> */}
           </Routes>
-        
-      </Router>
-    </Container>
+        </Router>
+      </Container>
     </div>
   );
 }
