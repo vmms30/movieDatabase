1aac098b3926c72970c75d5d89d72127c4c1ada3
Vincent-Soweto-biz <vincent.soweto.biz@gmail.com>
2025-06-17 17:08:14 +0200

    add navigation link to favourites page for the Navigation bar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
@@ -12,8 +12,15 @@ const Navigation = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/trending">Trending</Nav.Link>
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/trending">
+              Trending
+            </Nav.Link>
+            <Nav.Link as={Link} to="/favourites">
+              Favourites
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
